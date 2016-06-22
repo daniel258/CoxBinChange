03445f8b2c31bb6969582cdb48804d4e4725afc7
Daniel Nevo <danielnevo@gmail.com>
2016-06-22 13:36:40 -0400

    First commit of the package. Currently cpp functions only

---
+++ src/FindIntervalCPP.cpp
@@ -0,0 +1,61 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+// tm - event time
+// event - censoring indicator (1 event 0 no event)
+// ps - probabilities
+// beta- a value
+
+//data is assumed to be sorted by time
+// [[Rcpp::export]]
+IntegerVector FindIntervalCPP(double point, NumericMatrix w) {
+  int nrow = w.nrow();
+  int ncol = w.ncol();
+  
+  IntegerVector intervalW(nrow);
+  bool CondLocation;
+    for (int i = 0; i < nrow; ++i)
+  {
+      int j=0;
+      CondLocation = true;
+      while(CondLocation == true)
+      {
+      if (w(i,j) > point)
+      {
+        if(j==0)
+        {
+          intervalW[i] = 1;
+          CondLocation = false;
+        } else
+          {     
+          intervalW[i] = j + 1;
+          CondLocation = false;
+          }
+        } else if (j==ncol) {
+        intervalW[i] = ncol;
+        CondLocation = false;}
+      j += 1;
+      }
+  }
+        
+    return intervalW;
+  }
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+// /*** R
+// timesTwo(42)
+// */
---
+++ src/myF.cpp
@@ -0,0 +1,58 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+// tm - event time
+// event - censoring indicator (1 event 0 no event)
+// ps - probabilities
+// beta- a value
+
+//data is not assumed to be sorted by time
+// [[Rcpp::export]]
+double myF(double beta, NumericVector tm, LogicalVector event, NumericMatrix ps) {
+  int n = tm.size();
+  double denom=0;
+  double logDenom=0;
+  double logNumer=0;
+  double logLik=0;
+  int iCaseNum=-1;
+  NumericMatrix contrib=1 + ps*(exp(beta)-1);
+  
+  for (int i = 0; i < n; ++i)
+  {
+    if (event[i]) {
+      iCaseNum += 1;
+      logNumer += log(contrib(iCaseNum,i));
+      denom = contrib(iCaseNum,i);
+      for(int j = 0; j < n; ++j) {
+       if (tm[j]>tm[i]) {
+        denom += contrib(iCaseNum,j);
+        }
+        }
+     logDenom += log(denom);
+    } }
+  logLik = logNumer - logDenom; 
+  
+//NumericVector contrib=ps*exp(beta)+1-ps;
+//List aa; aa["logLik"] = logLik;
+//  return aa;
+    return logLik;
+  }
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+// /*** R
+// timesTwo(42)
+// */
