e4cd246670a7017272140b80321204a52af50f29
Daniel Nevo <danielnevo@gmail.com>
2017-02-21 11:08:00 -0500

    Change file name and minor comments

--- R/FitCalibWeibullRS.R
+++ R/FitCalibWeibullRS.R
@@ -1,4 +1,4 @@
-#### FitCalibWeibull function
+#### FitCalibWeibullRS function
 ### Daniel Nevo
 ## The function takes the following
 ## w - a matrix. Each row is observation and each column is questionnaire time in the interval. w equal to Inf once
@@ -7,7 +7,7 @@
 # second questionnaire result for all participents.
 ###
 # The function returns the estimates of Weibull scale and shape paramters for interval-censored time to event data.
-FitCalibWeibullRS <- function(w, w.res, obs.tm, event)
+FitCalibWeibullRS <- function(w, w.res, tm, event)
 {
 r <- sum(event)
 event.index <- which(event==1)
@@ -15,8 +15,8 @@ lr.for.fit.all <- as.data.frame(FindIntervalCalibCPP(w = w, wres = w.res))
 weib.params <- matrix(nr = r, nc = 2)
 for (j in 1:r)
 {
-  point <- obs.tm[event.index[j]]
-  lr.for.fit <- lr.for.fit.all[obs.tm>=point,]  # Keep only observations in the risk set
+  point <- tm[event.index[j]]
+  lr.for.fit <- lr.for.fit.all[tm>=point,]  # Keep only observations in the risk set
   lr.for.fit <- lr.for.fit[!(lr.for.fit[,1]==0 & lr.for.fit[,2]==Inf),]
   colnames(lr.for.fit) <- c("left","right")
   lr.for.fit[lr.for.fit==Inf] <- 200
