255749b741e64ce2ceb6b457272ece62e85677bc
Daniel Nevo <danielnevo@gmail.com>
2017-04-28 10:00:15 -0400

    Artificial bounds removed for FitCalibWeibull

--- R/OrdineryCalibration.R
+++ R/OrdineryCalibration.R
@@ -31,10 +31,10 @@ FitCalibWeibull <- function(w,w.res)
 FitCalibNpmle <- function(w,w.res)
 {
   lr.for.fit <- as.data.frame(FindIntervalCalibCPP(w = w, wres = w.res))
-  lr.for.fit <- lr.for.fit[!(lr.for.fit[,1]==0 & lr.for.fit[,2]==Inf),]
+  #lr.for.fit <- lr.for.fit[!(lr.for.fit[,1]==0 & lr.for.fit[,2]==Inf),]
   colnames(lr.for.fit) <- c("left","right")
-  lr.for.fit[lr.for.fit==Inf] <- 200
-  lr.for.fit[lr.for.fit==0] <- 0.0001
+  #lr.for.fit[lr.for.fit==Inf] <- 200
+  #lr.for.fit[lr.for.fit==0] <- 0.0001
   #df.cln <- lr.for.fit[apply(lr.for.fit,1,function(x)  sum(is.na(x)))<2,]
   fit.npmple <- ic_np(cbind(left,right)~0,data = lr.for.fit)
   if (inherits(fit.npmple, "error")) { 
