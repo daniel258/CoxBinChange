85bfc2e13b67d8eb871a9e07ebcbcdc950a5257a
Daniel Nevo <danielnevo@gmail.com>
2017-04-28 13:49:17 -0400

    Corrected Weibull calibration (ending point = NA for right censored)

--- R/OrdineryCalibration.R
+++ R/OrdineryCalibration.R
@@ -10,8 +10,8 @@ FitCalibWeibull <- function(w,w.res)
   lr.for.fit <- as.data.frame(FindIntervalCalibCPP(w = w, wres = w.res))
   lr.for.fit <- lr.for.fit[!(lr.for.fit[,1]==0 & lr.for.fit[,2]==Inf),]
   colnames(lr.for.fit) <- c("left","right")
-  lr.for.fit[lr.for.fit==Inf] <- 200
-  lr.for.fit[lr.for.fit==0] <- 0.0001
+  lr.for.fit[lr.for.fit==Inf] <- NA
+#  lr.for.fit[lr.for.fit==0] <- 0.0001
   #df.cln <- lr.for.fit[apply(lr.for.fit,1,function(x)  sum(is.na(x)))<2,]
   fit.weib <- tryCatch(fitdistcens(censdata = lr.for.fit, distr = "weibull"),   error=function(e) {e})
   if (inherits(fit.weib, "error")) { 
